23b2e90371687bf2957d518421fa1f6be56707a2
Mateusz Dragan <33156@student.pwsztar.edu.pl>
2021-11-08 18:16:57 +0100

    "Dodano szuakanie ilości generałów lojalnych i zdrajców"

--- project.py
+++ project.py
@@ -8,8 +8,31 @@ def make_generals(size):
     return gen
 
 
+def count_generals(gen):
+    t = 0
+    l = 0
+    i = 0
+    while i < len(gen):
+        if gen[i]:
+            l += 1
+        else:
+            t += 1
+        i += 1
+    return l,t
+
+
+def decide():
+    decision = True
+
+    return decision
+
+
 def main():
-    print(make_generals(5))
+    il = int(input("Podaj liczbę generałów: "))
+    gen = make_generals(il)
+    print(gen)
+    print("Ilość lojalnych generałów:",count_generals(gen)[0])
+    print("Ilość zdrajców wśród generałów:", count_generals(gen)[1])
 
 
 if __name__ == "__main__":
